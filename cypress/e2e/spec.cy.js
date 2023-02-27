58b74d7bbb85af9ddcd5b5d8b9a9beb21c74c7e4
Craig Drayton <craig.drayton@gmail.com>
2023-02-28 09:41:50 +1100

    Adds time-based test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,4 +6,9 @@ describe('hello world landing page loads', () => {
   it('has the boilerplate text', () => {
     cy.contains('Open up App.js')
   })
+
+  it('displays the current system time', () => {
+    cy.clock(new Date("2023-01-01T12:24:00"))
+    cy.contains("12:24:00 PM")
+  })
 })
