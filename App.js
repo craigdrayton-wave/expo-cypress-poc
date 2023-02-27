744cb4d8adb156f5361644f6c69d71bba86c1554
Craig Drayton <craig.drayton@gmail.com>
2023-02-28 10:40:37 +1100

    Adds example test with elapsed time

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const now = new Date
+  const [idle, setIdle] = useState(false);
+
+  useEffect(() => {
+    const idleTimer = setTimeout(() => {
+      setIdle(true)
+    }, 100)
+
+    return () => clearTimeout(idleTimer)
+  })
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Your local time is {now.toLocaleTimeString('en-US')}</Text>
+      {idle ? <Text>Are you still there?</Text> : null}
+      
       <StatusBar style="auto" />
     </View>
   );
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -11,4 +11,14 @@ describe('hello world landing page loads', () => {
     cy.clock(new Date("2023-01-01T12:24:00"))
     cy.contains("12:24:00 PM")
   })
+
+  it('displays a message after 1 hour has elapsed', () => {
+    const clock = cy.clock(new Date("2023-01-01T12:24:00"))
+
+    cy.contains('Open up App.js')
+    cy.contains("Are you still here?").should('not.exist')
+
+    clock.tick(3600001)
+    cy.contains("Are you still there?")
+  })
 })
