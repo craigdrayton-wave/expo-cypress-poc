e9512dad420e2b1f11b703c6dc4147ec5cce8f1f
Craig Drayton <craig.drayton@gmail.com>
2023-03-01 13:44:28 +1100

    Adds native base

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Center } from "native-base";
 
 export default function App() {
   const now = new Date
@@ -15,13 +16,15 @@ export default function App() {
   })
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Your local time is {now.toLocaleTimeString('en-US')}</Text>
-      {idle ? <Text>Are you still there?</Text> : null}
-      
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Your local time is {now.toLocaleTimeString('en-US')}</Text>
+        {idle ? <Text>Are you still there?</Text> : null}
+        
+        <StatusBar style="auto" />
+      </Center>
+    </NativeBaseProvider>
   );
 }
 
